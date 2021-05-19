4e7985fd5e107b3531f1ddcb9b2d8b7a39f87592
Feng Zhang <prod.feng@gmail.com>
2021-05-19 19:36:13 -0400

    Update zltop.py

--- zltop.py
+++ zltop.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 '''
-This purpose of this program is to provide a lighed weighted tool to monitor the Lustre storage's performance.
+This purpose of this program is to provide a light weighted tool to monitor Lustre storage's performance.
 It only uses ssh, lctl command, python's curses, multiprocess modules.
-Lustre seems has very rough manual for it's procfs system, many of them are not clear what they really are. We can still get 
+Lustre seems has very poor manual for it's procfs system, many of them are not clear what they really are. We can still get 
 some useful performance metrics from it.
 
 This program does not support multiple OSTs on an OSS, or multiple MDTs either. Some of the performance metrics are missing, which need more works.
